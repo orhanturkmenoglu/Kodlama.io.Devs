e58165014263b1bb07fe024653fca25301ec7047
orhanturkmenoglu <orhantrkmn749@gmail.com>
2022-11-21 21:55:20 +0300

    slf4 logging desteği sağlandı.

--- src/main/java/kodlama/io/Kodlama/io/Devs/Application.java
+++ src/main/java/kodlama/io/Kodlama/io/Devs/Application.java
@@ -1,31 +1,19 @@
 package kodlama.io.Kodlama.io.Devs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application implements CommandLineRunner {
-   /* @Autowired
-    private TecnologyRepository repository;*/
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("Spring boot application started");
     }
 
     @Override
     public void run(String... args) throws Exception {
-   /*     ProgrammingLanguage programmingLanguage = ProgrammingLanguage.builder()
-                .language("Java")
-                .build();
-
-        Tecnology tecnology  = Tecnology.builder()
-                .tecnologyName("Spring Framework")
-                .programmingLanguage(programmingLanguage)
-                .build();
-
-
-        repository.save(tecnology);*/
-
-
     }
 }
--- src/main/java/kodlama/io/Kodlama/io/Devs/business/ProgrammingLanguageService.java
+++ src/main/java/kodlama/io/Kodlama/io/Devs/business/ProgrammingLanguageService.java
@@ -8,6 +8,7 @@ import kodlama.io.Kodlama.io.Devs.business.response.TechnologyDto;
 import kodlama.io.Kodlama.io.Devs.entity.ProgrammingLanguage;
 import kodlama.io.Kodlama.io.Devs.repositories.ProgrammingLanguageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProgrammingLanguageService {
 
     private final TechnologyService technologyService;
@@ -26,27 +28,32 @@ public class ProgrammingLanguageService {
 
     @Transactional
     public ProgrammingLanguageDto createProgrammingLanguage(CreateProgrammingLanguageRequest request) {
+        log.info("ProgrammingLanguageService::createProgrammingLanguage started");
         ProgrammingLanguage programmingLanguage = programmingLanguageMapper.mapToProgrammingLanguage(request);
         ProgrammingLanguage savedProgrammingLanguage = programmingLanguageRepository.save(programmingLanguage);
         List<TechnologyDto> technologyList = technologyService.createTechnology(request.getTechnologyRequestList(), savedProgrammingLanguage.getId());
+        log.info("ProgrammingLanguageService::createProgrammingLanguage finished");
         return programmingLanguageMapper.mapToProgrammingLanguageDto(savedProgrammingLanguage, technologyList);
     }
     public List<ProgrammingLanguageDto> getAllProgrammingLanguages(String name) {
+        log.info("ProgrammingLanguageService::getAllProgrammingLanguages started");
         if (name != null && name.length() > 0) {
             List<ProgrammingLanguage> programmingLanguageList = programmingLanguageRepository.findBylanguage(name);
             return programmingLanguageMapper.mapToProgrammingLanguageDto(programmingLanguageList);
         }
 
         List<ProgrammingLanguage> programmingLanguageList = programmingLanguageRepository.findAll();
+        log.info("ProgrammingLanguageService::getAllProgrammingLanguages finished");
         return programmingLanguageMapper.mapToProgrammingLanguageDto(programmingLanguageList);
     }
     public ProgrammingLanguageDto findById(long id) {
+        log.info("ProgrammingLanguageService::findById started");
         Optional<ProgrammingLanguage> findById = programmingLanguageRepository.findById(id);
         ProgrammingLanguageDto response = new ProgrammingLanguageDto();
         if (findById.isPresent()) {
             programmingLanguageMapper.mapToProgrammingLanguageDto(findById.get());
         }
-
+        log.info("ProgrammingLanguageService::findById finished");
         return response;
     }
 
